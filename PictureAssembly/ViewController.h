85658cf5efdff5c1f974e2b8e3d790c8752ae25c
Yong Zeng <dustinzeng0608@gmail.com>
2016-11-09 01:10:25 -0500

    Initial Commit

---
+++ PictureAssembly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureAssembly
+//
+//  Created by Yong Zeng on 11/9/16.
+//  Copyright © 2016 Yong Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PictureAssemblyTests/PictureAssemblyTests.m
@@ -0,0 +1,39 @@
+//
+//  PictureAssemblyTests.m
+//  PictureAssemblyTests
+//
+//  Created by Yong Zeng on 11/9/16.
+//  Copyright © 2016 Yong Zeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PictureAssemblyTests : XCTestCase
+
+@end
+
+@implementation PictureAssemblyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
